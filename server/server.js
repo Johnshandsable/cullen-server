c8b9a571c1b7c53301c79dc67b0efd5caef704b9
Johnshandsable <Jshands@asu.edu>
2021-02-02 14:59:42 -0600

    completed running server on port 3000

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
 const express = require('express');
+// console.log(express);
 
-console.log(express);
+// Create our application
+const app = express();
+
+// Listen for network requests
+app.listen(3000, function () {
+  // When the server is ready, call this function
+  console.log('the app is listening at', 3000);
+});
