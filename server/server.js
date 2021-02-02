3fe90dcd1da868fe16123e8e3fe7900aff9085a4
Johnshandsable <Jshands@asu.edu>
2021-02-02 15:49:04 -0600

    added an index.html file for the server front page

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const express = require('express');
 // Create our application
 const app = express();
 
+// INCANTATION
+// Share any files inside the public folder
+app.use(express.static('server/public'));
+
 // Listen for network requests
 app.listen(3000, function () {
   // When the server is ready, call this function
